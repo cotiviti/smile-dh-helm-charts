8f00de2c6528be50ba6d5f8ae869a4756fdabe7f
Tommy Francis <tommy.francis@smilecdr.com>
2024-08-24 00:07:59 -0400

    ci(docs): add changelog per channel

--- scripts/semantic-release-prepare-changelog.sh
+++ scripts/semantic-release-prepare-changelog.sh
@@ -5,6 +5,12 @@
 # The semantic release `@semantic-release/changelog` plugin does not allow setting changelog filename using environment variables.
 # So instead, this script will do the following:
 
+# It creates a per-version, per-channel changelog file which gets updated by semantic release.
+# This file is then copied to the main per-version changelog for inclusion in the docs.
+# The result of this is that the per-version changelog will get all the changes in a given channel.
+# When a version is promoted to a new channel, then it will start with a fresh changelog file,
+#  but will still be populated with all the changes to date. Nifty!
+
 # When run with `pre-prepare`:
 # * Determine current major version
 # * Copy/move the changelog for current major version to CHANGELOG-TMP.md
@@ -35,28 +41,29 @@ NEW_MAJOR="$(cut -d '.' -f 1 <<< "${NEW_VER}")"
 
 # TEMP_CHANGELOG_FILE="dist/CHANGELOG-TMP.md"
 TEMP_CHANGELOG_FILE="CHANGELOG-TMP.md"
+CHANGELOG_FILE="CHANGELOG-V${NEW_MAJOR}.md"
+CHANNEL_CHANGELOG_FILE="CHANGELOG-V${NEW_MAJOR}-${CHANNEL}.md"
 
 
 if [ "pre-prepare" == "${MODE}" ]; then
     echo "Preparing V${NEW_MAJOR} changelog file for the 'prepare' step of the update plugin "
-    CHANGELOG_FILE="CHANGELOG-V${NEW_MAJOR}.md"
-    if [ -f "${CHANGELOG_FILE}" ]; then
+    if [ -f "${CHANNEL_CHANGELOG_FILE}" ]; then
         mkdir -p "dist"
-        mv "${CHANGELOG_FILE}" "${TEMP_CHANGELOG_FILE}"
+        mv "${CHANNEL_CHANGELOG_FILE}" "${TEMP_CHANGELOG_FILE}"
     else
-        echo "${CHANGELOG_FILE} does not exist. A new changelog file will be created for V${NEW_MAJOR}"
+        echo "${CHANNEL_CHANGELOG_FILE} does not exist. A new changelog file will be created for V${NEW_MAJOR}in the ${CHANNEL} channel"
     fi
 elif [ "post-prepare" == "${MODE}" ]; then
     echo "Preparing V${NEW_MAJOR} changelog file for 'prepare' step of the git plugin"
-    CHANGELOG_FILE="CHANGELOG-V${NEW_MAJOR}.md"
     # Ensure that the versioned changelog file is not there. It should have been MOVED by the prepare command above
-    if [ -f "${CHANGELOG_FILE}" ]; then
-        echo "Error: ${CHANGELOG_FILE} already exists but should not. Maybe the 'prepare' command was not called beforehand"
+    if [ -f "${CHANNEL_CHANGELOG_FILE}" ]; then
+        echo "Error: ${CHANNEL_CHANGELOG_FILE} already exists but should not. Maybe the 'prepare' command was not called beforehand"
         exit 1
     fi
     if [ -f "${TEMP_CHANGELOG_FILE}" ]; then
-        mv "${TEMP_CHANGELOG_FILE}" ${CHANGELOG_FILE}
+        mv "${TEMP_CHANGELOG_FILE}" "${CHANNEL_CHANGELOG_FILE}"
         ls
+        cp "${CHANNEL_CHANGELOG_FILE}" "${CHANGELOG_FILE}"
     else
         echo "Error: No changelog file (${TEMP_CHANGELOG_FILE}) was created by @semantic-release/changelog."
         exit 1
